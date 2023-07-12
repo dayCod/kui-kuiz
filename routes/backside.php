3b1c41433c69ae324b10f3f20c591e9a65702a8e
dayCod <alayarahman07@gmail.com>
2023-07-12 11:44:40 +0700

    Update: Admin Prefix

--- routes/backside.php
+++ routes/backside.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::group(['middleware' => ['guest']], function () {
+Route::group(['prefix' => 'admin', 'middleware' => ['guest']], function () {
 
     /*
     |--------------------------------------------------------------------------
