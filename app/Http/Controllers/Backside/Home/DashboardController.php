0015ec79b3d28d15b76616d3aa816b94146cb42a
dayCod <alayarahman07@gmail.com>
2023-08-09 14:55:40 +0700

    Documentation: Text Comment Dashboard Controller

--- app/Http/Controllers/Backside/Home/DashboardController.php
+++ app/Http/Controllers/Backside/Home/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Backside\Home;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    /**
+     * display the home page of admin dashboard
+     *
+     * @return View
+     */
+    public function index(): View
     {
         return view('backside.page.dashboard.index');
     }
